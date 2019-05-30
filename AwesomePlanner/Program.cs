d3209d7f9e5740e887e2ccfd3f20a154d0198923
Surplus <yingouyingou@gmail.com>
2019-05-30 12:55:37 +0900

    add AwesomePlanner 콘솔창이 안떠서 수정함 (1)

--- AwesomePlanner/Program.cs
+++ AwesomePlanner/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AwesomePlanner
 {
@@ -6,7 +10,8 @@ namespace AwesomePlanner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
         }
     }
 }
